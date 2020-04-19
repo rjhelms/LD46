4274a06c5e97e0433e316fd52c8803a55a533b22
Rob Hailman <robhailman@gmail.com>
2020-04-19 15:13:42 -0400

    Add Bar
    powerup spawner. To be tweaked

---
+++ Assets/Scripts/PowerupSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] powerUpArray;
+
+    [SerializeField]
+    private Transform spawnPosition;
+
+    [SerializeField]
+    private Transform powerupContainer;
+
+    [SerializeField]
+    private float spawnFrequency;
+
+    [SerializeField]
+    private float spawnVariance;
+
+    private bool hasChild;
+    private float nextSpawnTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasChild)
+        {
+            if (powerupContainer.childCount == 0)
+            {
+                hasChild = false;
+                nextSpawnTime = Time.time + (1 / (spawnFrequency * Random.Range(1 - spawnVariance, 1 + spawnVariance)));
+            }
+        } else if (Time.time > nextSpawnTime)
+        {
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        GameObject toSpawn = powerUpArray[Random.Range(0, powerUpArray.Length)];
+        Instantiate(toSpawn, spawnPosition.position, Quaternion.identity, powerupContainer);
+        hasChild = true;
+    }
+}
