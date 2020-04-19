8408bcaa2797ad85da782d32e9e7c9bc147197f7
Rob Hailman <robhailman@gmail.com>
2020-04-18 20:21:30 -0400

    Add follow camera
    I don't really like how this feels

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+    [SerializeField]
+    private Vector3 offset;
+    [SerializeField]
+    private float interpMagnitude;
+    [SerializeField]
+    private float lerpAmount;
+
+    private float interpVelocity;
+    private Vector3 targetPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (target)
+        {
+            Vector3 posNoZ = transform.position;
+            posNoZ.z = target.transform.position.z;
+
+            Vector3 targetDirection = (target.transform.position - posNoZ);
+            interpVelocity = targetDirection.magnitude * interpMagnitude;
+            Debug.Log(targetDirection);
+            Debug.Log(interpVelocity);
+            targetPos = posNoZ + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+        }
+    }
+}
