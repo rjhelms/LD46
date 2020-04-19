72027036635162907ba49ef5f23e941058ed0b72
Rob Hailman <robhailman@gmail.com>
2020-04-19 11:26:50 -0400

    Add powerup art and prefabs
    Code is just stub class for now

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    [SerializeField]
+    private int powerUp;
+
+    [SerializeField]
+    private int mansUp;
+
+    [SerializeField]
+    private int scoreUp;
+
+    [SerializeField]
+    private AudioClip audioClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
