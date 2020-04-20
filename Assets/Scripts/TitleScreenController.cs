0e2f38ff57f0de48e31e17bb3d1a5b086d867d0a
Rob Hailman <robhailman@gmail.com>
2020-04-19 21:02:35 -0400

    Add title screen - done except for packaging!

---
+++ Assets/Scripts/TitleScreenController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenController : MonoBehaviour
+{
+    public GameObject audioManager;
+    private float accessTime;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (AudioManager.instance == null)
+        {
+            Instantiate(audioManager);
+        }
+    }
+
+    void Start()
+    {
+        accessTime = Time.time + 0.25f;
+    }
+
+    void Update()
+    {
+        if (Time.time > accessTime)
+        {
+            if (Input.anyKey)
+            {
+                SceneManager.LoadScene("Main");
+            }
+        }
+    }
+}
